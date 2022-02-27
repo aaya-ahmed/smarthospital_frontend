92205ce9114608357c687d07ea61fd6d8e3b9c96
aaya-ahmed <aaya7098@gmail.com>
2022-02-27 11:13:34 -0700

    new

--- src/app/modules/patient/patient.component.ts
+++ src/app/modules/patient/patient.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+declare const  togglebar:any;
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',
   styleUrls: ['./patient.component.css']
 })
 export class PatientComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  navbartoggle=false;
+  dropdowntoggle=false;
+   activecomponent:string='';
+  constructor(private active:ActivatedRoute) { 
+    
   }
 
+  ngOnInit(): void {
+
+  }
+  namecomponent(name_component:string){
+    this.activecomponent=name_component;
+  }
+  togglenavbar(){
+    this.navbartoggle=!this.navbartoggle;
+  }
+  toggledropdown(){
+    this.dropdowntoggle=!this.dropdowntoggle;
+  }
+  togglebar(){
+    togglebar();
+}  
 }
--- src/app/modules/patient/patient.module.ts
+++ src/app/modules/patient/patient.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PatientRoutingModule } from './patient-routing.module';
-import { PatientComponent } from './patient/patient.component';
+import { PatientComponent } from '../patient/patient.component';
 
 
 @NgModule({
